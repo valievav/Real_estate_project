bacb97934f4c5c48fd1e30121a4fc6073923e540
Venus <valievavm@gmail.com>
2020-08-06 19:20:42 +0200

    Customize admin view for listings and realtors

--- real_estate/listings/admin.py
+++ real_estate/listings/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'city', 'state', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('city', 'state', 'realtor')
+    list_editable = ('is_published', 'price')
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zip_code', 'price')
+    list_per_page = 20
+
+
+admin.site.register(Listing, ListingAdmin)
--- real_estate/realtors/admin.py
+++ real_estate/realtors/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Realtor
 
-admin.site.register(Realtor)
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone', 'email', 'is_mvp', 'hire_date')
+    list_display_links = ('id', 'name')
+    list_editable = ('is_mvp', )
+    search_fields = ('name', )
+    list_per_page = 20
+
+
+admin.site.register(Realtor, RealtorAdmin)
