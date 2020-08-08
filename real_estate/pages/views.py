40aef2a250906590357681b46750102ed158faf6
Venus <valievavm@gmail.com>
2020-08-08 16:11:32 +0200

    Pull db data for home and about pages

--- real_estate/pages/views.py
+++ real_estate/pages/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing, Realtor
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
+    context = {
+        "listings": listings
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by("-is_mvp", "hire_date")
+    mvp_realtors = Realtor.objects.filter(is_mvp=True)
+
+    context = {
+        "realtors": realtors,
+        "mvp_realtors": mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
