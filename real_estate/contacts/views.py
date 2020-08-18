09f0f14bd8eb38c2dfa01f7758493de96378e32f
Venus <valievavm@gmail.com>
2020-08-18 07:57:47 +0200

    Comment sending email section, update readme

--- real_estate/contacts/views.py
+++ real_estate/contacts/views.py
@@ -36,20 +36,20 @@ def contact(request):
                 ).save()
 
         # send email to realtor
-        send_mail(
-            f'New inquiry for {listing}',
-            f'Hello :)\n\n'
-            f'Please see inquiry details below:\n'
-            f'Property: {listing}\n'
-            f'User: {name}\n'
-            f'Phone: {phone}\n'
-            f'Email: {email}\n'
-            f'Is_registered: {True if user_id else False}\n'
-            f'Message: {message}\n',
-            'real_estate@website.com',
-            [realtor_email],
-            fail_silently=False,
-        )
+        # send_mail(
+        #     f'New inquiry for {listing}',
+        #     f'Hello :)\n\n'
+        #     f'Please see inquiry details below:\n'
+        #     f'Property: {listing}\n'
+        #     f'User: {name}\n'
+        #     f'Phone: {phone}\n'
+        #     f'Email: {email}\n'
+        #     f'Is_registered: {True if user_id else False}\n'
+        #     f'Message: {message}\n',
+        #     'real_estate@website.com',
+        #     [realtor_email],
+        #     fail_silently=False,
+        # )
 
         messages.success(request, 'Your request has been submitted. Realtor will get back to you as soon as possible.')
         return redirect('/listings/'+listing_id)
